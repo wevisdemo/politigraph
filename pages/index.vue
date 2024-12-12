f2f3dfe801589ed6d598ddf47dd5f1af986c2fa3
Th1nkK1D <witheep@gmail.com>
2024-12-12 16:49:24 +0700

    chore: migrate to nuxtjs

---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+	<h1>Politigraph</h1>
+</template>
--- server/routes/graphql.ts
+++ server/routes/graphql.ts
@@ -2,13 +2,13 @@ import { ApolloServer } from '@apollo/server';
 import { startServerAndCreateH3Handler } from '@as-integrations/h3';
 import { Neo4jGraphQL } from '@neo4j/graphql';
 import neo4j from 'neo4j-driver';
-import { typeDefs } from '../schemas/graph';
+import { typeDefs } from '../../schemas/graph';
 
 const driver = neo4j.driver(
 	'neo4j://localhost:7687',
 	neo4j.auth.basic(
-		import.meta.env.NEO4J_USERNAME as string,
-		import.meta.env.NEO4J_PASSWORD as string,
+		process.env.NEO4J_USERNAME as string,
+		process.env.NEO4J_PASSWORD as string,
 	),
 );
 
@@ -16,9 +16,9 @@ const schema = await new Neo4jGraphQL({
 	typeDefs,
 	driver,
 	features: {
-		authorization: import.meta.env.JWT_SECRET
+		authorization: process.env.JWT_SECRET
 			? {
-					key: import.meta.env.JWT_SECRET,
+					key: process.env.JWT_SECRET,
 				}
 			: undefined,
 	},
@@ -30,7 +30,7 @@ export default startServerAndCreateH3Handler(
 	}),
 	{
 		context: async (req) => ({
-			token: req.event.headers.get('authorization').replace('Bearer ', ''),
+			token: req.event.headers.get('authorization')?.replace('Bearer ', ''),
 		}),
 	},
 );
