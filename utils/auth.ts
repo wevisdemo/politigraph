0dba28d3153936b5e216f93693288e1b13f98005
Th1nkK1D <witheep@gmail.com>
2025-01-09 16:05:27 +0700

    fix: auto create better auth folder and accept migration

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,13 +1,20 @@
+import { existsSync, mkdirSync } from 'fs';
 import { betterAuth } from 'better-auth';
 import { jwt, openAPI } from 'better-auth/plugins';
 import Database from 'better-sqlite3';
+
+const BETTER_AUTH_PATH = '.better-auth';
+
+if (!existsSync(BETTER_AUTH_PATH)) {
+	mkdirSync(BETTER_AUTH_PATH);
+}
 
 process.env.BETTER_AUTH_URL =
 	process.env.NUXT_PUBLIC_BASE_URL || 'http://localhost:3000';
 
 export const auth = betterAuth({
 	basePath: 'auth',
-	database: new Database('.better-auth/sqlite.db'),
+	database: new Database(`${BETTER_AUTH_PATH}/sqlite.db`),
 	plugins: [
 		jwt(),
 		...(process.env.NODE_ENV !== 'production' ? [openAPI()] : []),
