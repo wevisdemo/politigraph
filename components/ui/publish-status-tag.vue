aa21f8984a26a2bdea6a2fc1bee386f5d0fd24c4
bariyard <worawitrr@gmail.com>
2025-04-25 17:41:58 +0700

    feat: vote event mutation test

---
+++ components/ui/publish-status-tag.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const props = defineProps({
+	status: {
+		type: String,
+		required: true,
+	},
+});
+
+const getPublishTitle = (status: string) => {
+	switch (status) {
+		case 'PUBLISHED':
+			return 'Published';
+		default:
+			return '';
+	}
+};
+</script>
+
+<template>
+	<div
+		class="flex flex-row gap-2 rounded-full bg-tag-background w-fit !p-2 items-center"
+	>
+		<div class="rounded-full size-4 bg-tag-icon" />
+		<div class="text-[12px] !capitalize">
+			{{ getPublishTitle(status) }}
+		</div>
+	</div>
+</template>
--- pages/admin/voting/[id]/index.vue
+++ pages/admin/voting/[id]/index.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
-import type { Vote, VoteEvent, VoteEventUpdateInput } from '~/.genql';
+//@ts-ignore
+import Save16 from '@carbon/icons-vue/es/save/16';
+//@ts-ignore
+import ViewOff16 from '@carbon/icons-vue/es/view--off/16';
+import type { VoteEventUpdateInput } from '~/.genql';
+import { graphqlClient } from '~/utils/graphql/client';
 
+const { getSession } = useAuthClient();
 definePageMeta({
 	layout: 'admin-layout',
 });
@@ -10,18 +16,8 @@ const route = useRoute();
 const isShowNotification = ref(true);
 const isShowNotificationError = ref(true);
 
-const voteFormInput = ref<VoteEventUpdateInput>({
-	title: '',
-	// nickname: '',
-	// date: '',
-	// involving_assembly: '',
-	result: '',
-	// winning_condition: '',
-	description: '',
-	// source_url: '',
-	// related_document: '',
-	// document_url: '',
-});
+const isFormDirty = ref(false);
+const voteEventFormInput = ref<VoteEventUpdateInput>({});
 
 const paginationData = ref({
 	page: 1,
@@ -29,10 +25,8 @@ const paginationData = ref({
 });
 const totalCount = ref(0);
 
-const VoteEventData = ref<VoteEvent>();
-const VoteList = ref<Vote[]>([]);
-const {} = useAsyncData(
-	'voteEventsConnection',
+const { data: VoteEventData } = useAsyncData(
+	'VoteEventData',
 	async () => {
 		const { voteEvents } = await graphqlClient.query({
 			voteEvents: {
@@ -46,34 +40,89 @@ const {} = useAsyncData(
 				id: true,
 				title: true,
 				start_date: true,
-				created_at: true,
 				result: true,
+				description: true,
+
 				publish_status: true,
 				classification: true,
-				votes: {
-					__args: {
-						sort: [
-							{
-								vote_order: 'ASC',
-							},
-						],
-						limit: 20,
-						offset: 0,
-					},
-					id: true,
-					vote_order: true,
-					voter_name: true,
-					voter_party: true,
-					option: true,
-				},
+				disagree_count: true,
+				agree_count: true,
+				abstain_count: true,
+				novote_count: true,
 			},
 		});
 		if (voteEvents.length > 0) {
-			const event = voteEvents[0] as VoteEvent;
-			VoteEventData.value = event;
-			VoteList.value = event.votes;
-			voteFormInput.value.title = event.title;
+			const event = voteEvents[0];
+			voteEventFormInput.value = {
+				title: event.title,
+				start_date: event.start_date,
+				result: event.result,
+				description: event.description,
+
+				disagree_count: event.disagree_count,
+				agree_count: event.agree_count,
+				abstain_count: event.abstain_count,
+				novote_count: event.novote_count,
+			};
+
+			// isFormDirty.value = false;
 		}
+		return voteEvents[0];
+	},
+	{ watch: [paginationData.value], server: false },
+);
+
+const { data: VoteList } = useAsyncData(
+	'VoteList',
+	async () => {
+		const { votes } = await graphqlClient.query({
+			votes: {
+				__args: {
+					limit: 20,
+					offset: 0,
+					where: {
+						vote_events_ALL: {
+							id_EQ: route.params.id as string,
+						},
+					},
+					sort: [
+						{
+							vote_order: 'ASC',
+						},
+					],
+				},
+				id: true,
+				voter_name: true,
+				voter_party: true,
+				voters: {
+					firstname: true,
+					lastname: true,
+				},
+				badge_number: true,
+				option: true,
+			},
+		});
+		return votes;
+	},
+	{ watch: [paginationData.value], server: false },
+);
+
+const { data: OrganizationList } = useAsyncData(
+	'OrganizationList',
+	async () => {
+		const { organizations } = await graphqlClient.query({
+			organizations: {
+				__args: {
+					where: {
+						classification_IN: ['HOUSE_OF_SENATE', 'HOUSE_OF_REPRESENTATIVE'],
+					},
+				},
+				id: true,
+				name: true,
+				classification: true,
+			},
+		});
+		return organizations;
 	},
 	{ watch: [paginationData.value], server: false },
 );
@@ -84,6 +133,10 @@ const handlePageChange = (page: number) => {
 
 const handlePageSizeChange = (pageSize: number) => {
 	paginationData.value.pageSize = pageSize;
+};
+
+const handleChangeInput = () => {
+	isFormDirty.value = true;
 };
 </script>
 
@@ -126,11 +179,61 @@ query Votes($limit: Int) {
 			<cv-breadcrumb-item class="text-[#0F62FE]">Voting</cv-breadcrumb-item>
 			<cv-breadcrumb-item>{{ VoteEventData?.title }}</cv-breadcrumb-item>
 		</cv-breadcrumb>
+		<div class="flex flex-row justify-between">
+			<div class="basis-1/2 flex flex-row items-center gap-6">
+				<h3 class="!font-normal !mb-12 !mt-4">
+					{{ VoteEventData?.title }}
+				</h3>
+				<div>
+					<ui-publish-status-tag
+						:status="VoteEventData?.publish_status || ''"
+					/>
+				</div>
+			</div>
+			<div class="flex justify-between items-center gap-4">
+				<cv-button
+					default="Save Changes"
+					:icon="Save16"
+					:disabled="!isFormDirty"
+					@click="
+						async () => {
+							console.log(voteEventFormInput);
 
-		<h3 class="!font-normal !mb-12 !mt-4">
-			{{ VoteEventData?.title }}
-		</h3>
-
+							const { updateVoteEvents } = await graphqlClient.mutation({
+								updateVoteEvents: {
+									__args: {
+										where: {
+											id_EQ: route.params.id as string,
+										},
+										update: {
+											...voteEventFormInput,
+										},
+									},
+									voteEvents: {
+										id: true,
+									},
+								},
+							});
+							console.log(updateVoteEvents);
+						}
+					"
+					>Save Changes</cv-button
+				>
+				<cv-button
+					default="Unpublished"
+					:icon="ViewOff16"
+					kind="tertiary"
+					@click="
+						async () => {
+							// const { data: session, error } = await getSession();
+							// if (session !== null && session.user.email)
+							// 	getPostToken(session.user.email, 'worawit2025');
+						}
+					"
+					>Unpublished</cv-button
+				>
+			</div>
+		</div>
 		<cv-inline-notification
 			v-if="isShowNotification"
 			lowContrast
@@ -150,54 +253,69 @@ query Votes($limit: Int) {
 
 		<div class="flex gap-8">
 			<div class="bg-white !p-4 basis-2/4">
-				<div class="flex justify-between items-center">
-					<h4>Voting Details</h4>
-					<div>
-						<cv-button default="Save Changes">Save Changes</cv-button>
+				<form class="flex flex-col gap-6" @submit.prevent="() => {}">
+					<div class="flex justify-between items-center">
+						<h4>Voting Details</h4>
 					</div>
-				</div>
-
-				<form @submit.prevent="() => {}">
 					<div class="!mb-3">
-						<cv-text-input label="Title" placeholder=""></cv-text-input>
-					</div>
-
-					<div class="!mb-3">
-						<cv-text-input label="Nickname" placeholder=""> </cv-text-input>
-					</div>
-
-					<div class="!mb-3">
-						<cv-date-picker
-							dateLabel="Date label"
-							invalidMessage=""
-							:value="new Date().toLocaleDateString()"
-						>
-						</cv-date-picker>
+						<cv-text-input
+							label="Title"
+							placeholder=""
+							v-model="voteEventFormInput.title"
+							@change="handleChangeInput"
+						/>
+						<div>{{ voteEventFormInput.title }} {{ isFormDirty }}</div>
 					</div>
 
 					<div class="!mb-3">
 						<cv-text-input
-							label="Involving Assembly(MultiSelect) query ทั้งหมด "
+							label="Nickname"
 							placeholder=""
+							modelValue="no-field-in-database"
+							@change="handleChangeInput"
 						>
 						</cv-text-input>
 					</div>
 
 					<div class="!mb-3">
-						<cv-select label="Result">
-							<cv-select-option
-								disabled
-								selected
-								hidden
-								value="placeholder-item"
-								>Choose an option</cv-select-option
-							>
+						<cv-date-picker
+							dateLabel="Start Date"
+							invalidMessage=""
+							v-model="voteEventFormInput.start_date"
+							@change="handleChangeInput"
+						>
+						</cv-date-picker>
+					</div>
+
+					<div class="!mb-3">
+						<cv-multi-select
+							title="Involving Assembly(MultiSelect) query ทั้งหมด "
+							placeholder=""
+							:options="
+								OrganizationList && OrganizationList.length > 0
+									? OrganizationList.map((d) => ({
+											label: d.name,
+											value: d.id,
+										}))
+									: []
+							"
+							v-model="voteEventFormInput.organizations"
+							@change="handleChangeInput"
+						/>
+					</div>
+
+					<div class="!mb-3">
+						<cv-select
+							label="Result"
+							v-model="voteEventFormInput.result"
+							@change="handleChangeInput"
+						>
+							<cv-select-option value="`null`">ไม่รู้ผล</cv-select-option>
 							<cv-select-option value="ผ่าน">ผ่าน</cv-select-option>
 							<cv-select-option value="ไม่ผ่าน">ไม่ผ่าน</cv-select-option>
 						</cv-select>
 					</div>
-
-					<div class="!mb-3">
+					<!-- <div class="!mb-3">
 						<cv-select label="Winning Condition">
 							<cv-select-option
 								disabled
@@ -210,10 +328,15 @@ query Votes($limit: Int) {
 								>ได้เสียงข้างมากในที่ประชุม</cv-select-option
 							>
 						</cv-select>
-					</div>
+					</div> -->
 
 					<div class="!mb-3">
-						<cv-text-area label="Description" placeholder=""> </cv-text-area>
+						<cv-text-area
+							label="Description"
+							placeholder=""
+							v-model="voteEventFormInput.description"
+							@change="handleChangeInput"
+						/>
 					</div>
 
 					<div class="!mb-3">
@@ -239,18 +362,49 @@ query Votes($limit: Int) {
 							>Add Another Item</cv-button
 						>
 					</div>
-
-					<div class="!mb-3 text-right">
-						<cv-button default="Save Changes">Save Changes</cv-button>
-					</div>
 				</form>
 			</div>
 
-			<div class="basis-2/4">
-				<cv-data-table
-					title="Voting (ต้องแก้ไขได้)"
-					helperText="Vote Summary: เห็นด้วย 400, ไม่เห็นด้วย 100, งดออกเสียง 2"
-				>
+			<div class="bg-white basis-2/4">
+				<div class="!p-4 flex flex-col gap-2">
+					<h4>Vote Summary (Original)</h4>
+					<div class="text-body-01 !mb-6">
+						ข้อมูลสรุปผลคะแนนที่ OCR จากหัวเอกสารบันทึกผลการลงมติ
+						โดยระบบจะใช้ผลคะแนนนี้ในการตรวจสอบข้อมูลการลงมติ (Votes)
+						ว่าถูกต้องตรงกันหรือไม่
+					</div>
+					<div class="grid grid-cols-2 gap-x-12 gap-y-4">
+						<div class="flex flex-row items-center gap-4">
+							<div class="basis-1/2">เห็นด้วย</div>
+							<cv-number-input
+								placeholder=""
+								v-model="voteEventFormInput.agree_count"
+							/>
+						</div>
+						<div class="flex flex-row items-center gap-4">
+							<div class="basis-1/2">ไม่เห็นด้วย</div>
+							<cv-number-input
+								placeholder=""
+								v-model="voteEventFormInput.disagree_count"
+							/>
+						</div>
+						<div class="flex flex-row items-center gap-4">
+							<div class="basis-1/2">งดออกเสียง</div>
+							<cv-number-input
+								placeholder=""
+								v-model="voteEventFormInput.abstain_count"
+							/>
+						</div>
+						<div class="flex flex-row items-center gap-4">
+							<div class="basis-1/2">ไ่ม่ลงคะแนน</div>
+							<cv-number-input
+								placeholder=""
+								v-model="voteEventFormInput.novote_count"
+							/>
+						</div>
+					</div>
+				</div>
+				<cv-data-table title="Votes" helperText="">
 					<template #headings>
 						<cv-data-table-heading
 							id="sb-title"
