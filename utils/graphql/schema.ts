d92214f78505f86632678f4fc3c8940eb981eb30
Th1nkK1D <witheep@gmail.com>
2025-07-07 19:04:04 +0700

    refactor: convert all scripts to typescript

--- utils/graphql/schema.ts
+++ utils/graphql/schema.ts
@@ -4,7 +4,7 @@ const SCHEMAS_DIR = 'schemas';
 
 export function getGraphqlTypeDefs() {
 	return readdirSync(SCHEMAS_DIR, { recursive: true })
-		.filter((file) => file.endsWith('.graphql'))
+		.filter((file) => (file as string).endsWith('.graphql'))
 		.map((schemaFile) => readFileSync(`${SCHEMAS_DIR}/${schemaFile}`, 'utf-8'))
 		.join('\n');
 }
