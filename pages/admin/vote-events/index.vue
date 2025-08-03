4d5251ed61186da4e10fad3a23eda33f43690ff2
JaeAiKay <78583537+JaeAiKay@users.noreply.github.com>
2025-08-04 00:07:34 +0700

    feat: filter on vote event

--- pages/admin/vote-events/index.vue
+++ pages/admin/vote-events/index.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 //@ts-ignore
 import { DocumentPdf16, NotebookReference16 } from '@carbon/icons-vue';
+import { enumPublishStatus, enumVoteEventType } from '~/.genql';
 import { graphqlClient } from '~/utils/graphql/client';
 import dayjs from 'dayjs';
 import { ref } from 'vue';
+import type { LocationQueryValue } from 'vue-router';
 
 definePageMeta({
 	layout: 'admin-layout',
@@ -13,17 +15,58 @@ useHead({
 	title: 'Vote Events | Politigraph Admin',
 });
 
+const router = useRouter();
+const route = useRoute();
+
 const paginationData = ref({
-	page: 1,
-	pageSize: 50,
+	page: Number(route.query.page ?? 1),
+	pageSize: Number(route.query.pageSize ?? 50),
+});
+
+const getStringQueryParam = (
+	val: LocationQueryValue | LocationQueryValue[] | undefined,
+	fallback: string = '',
+): string => {
+	if (Array.isArray(val)) return val[0] ?? fallback;
+	return val ?? fallback;
+};
+
+const getArrayQueryParam = (
+	val: LocationQueryValue | LocationQueryValue[] | undefined,
+): string[] => {
+	if (Array.isArray(val)) return val.filter(Boolean) as string[];
+	return val ? [val] : [];
+};
+
+const filters = ref({
+	assembly: getStringQueryParam(route.query.assembly, 'ALL'),
+	status: getStringQueryParam(route.query.status, 'ALL'),
+	classification: getArrayQueryParam(route.query.classification),
 });
 
 const { data } = await useAsyncData(
 	'voteEvents',
 	async () => {
+		const where: Record<string, any> = {};
+
+		if (filters.value.assembly !== 'ALL') {
+			where.organizations_SOME = {
+				id_EQ: filters.value.assembly,
+			};
+		}
+
+		if (filters.value.status !== 'ALL') {
+			where.publish_status_EQ = filters.value.status;
+		}
+
+		if (filters.value.classification.length > 0) {
+			where.classification_IN = filters.value.classification;
+		}
+
 		const { voteEvents, voteEventsConnection } = await graphqlClient.query({
 			voteEvents: {
 				__args: {
+					where,
 					sort: [{ start_date: 'DESC' }, { created_at: 'DESC' }],
 					limit: paginationData.value.pageSize,
 					offset:
@@ -37,6 +80,7 @@ const { data } = await useAsyncData(
 				publish_status: true,
 				classification: true,
 				organizations: {
+					id: true,
 					abbreviation: true,
 					term: true,
 				},
@@ -56,15 +100,36 @@ const { data } = await useAsyncData(
 				},
 			},
 		});
+
 		return {
 			voteEvents,
 			totalCount: voteEventsConnection.aggregate.count.nodes,
 		};
 	},
 	{
-		watch: [paginationData.value],
+		watch: [paginationData.value, filters.value],
 		server: false,
 	},
+);
+
+const { data: organizations } = await useAsyncData(
+	'OrganizationList',
+	async () => {
+		const result = await graphqlClient.query({
+			organizations: {
+				__args: {
+					where: {
+						classification_IN: ['HOUSE_OF_REPRESENTATIVE', 'HOUSE_OF_SENATE'],
+					},
+				},
+				id: true,
+				abbreviation: true,
+				term: true,
+			},
+		});
+		return result.organizations ?? [];
+	},
+	{ server: false },
 );
 
 const numberOfPage = computed(() =>
@@ -81,11 +146,48 @@ const handlePageSizeChange = (pageSize: number) => {
 	paginationData.value.pageSize = pageSize;
 };
 
-const router = useRouter();
+const statusOption = Object.values(enumPublishStatus);
+const classificationOption = Object.values(enumVoteEventType);
+const voteEventType: Record<string, string> = {
+	MP_1: 'สส. วาระที่ 1',
+	MP_2: 'สส. วาระที่ 2',
+	MP_3: 'สส. วาระที่ 3',
+	SENATE_1: 'สว. วาระที่ 1',
+	SENATE_2: 'สว. วาระที่ 2',
+	SENATE_3: 'สว. วาระที่ 3',
+};
+
+watch(
+	[filters, paginationData],
+	() => {
+		router.replace({
+			query: {
+				...route.query,
+				assembly:
+					filters.value.assembly !== 'ALL' ? filters.value.assembly : undefined,
+				status:
+					filters.value.status !== 'ALL' ? filters.value.status : undefined,
+				classification:
+					filters.value.classification.length > 0
+						? filters.value.classification
+						: undefined,
+				page:
+					paginationData.value.page !== 1
+						? paginationData.value.page
+						: undefined,
+				pageSize:
+					paginationData.value.pageSize !== 50
+						? paginationData.value.pageSize
+						: undefined,
+			},
+		});
+	},
+	{ deep: true },
+);
 </script>
 
 <template>
-	<div class="bg-[#F4F4F4] p-10 pt-[90px]">
+	<div class="relative bg-[#F4F4F4] p-10 pt-[90px]">
 		<cv-breadcrumb noTrailingSlash>
 			<cv-breadcrumb-item>Vote Events</cv-breadcrumb-item>
 		</cv-breadcrumb>
@@ -95,96 +197,127 @@ const router = useRouter();
 			title="Vote Events"
 			helperText="การลงมติทั้งหมด"
 		></cv-data-table-skeleton>
-		<div v-else>
-			<cv-data-table title="Vote Events" helperText="การลงมติทั้งหมด">
-				<template #headings>
-					<cv-data-table-heading
-						id="sb-title"
-						heading="Title"
-						order="ascending"
-						class="w-2xl"
-					/>
-					<cv-data-table-heading
-						id="sb-votingdate"
-						heading="Voting Date"
-						class="text-center"
-					/>
-					<cv-data-table-heading
-						id="sb-createdat"
-						heading="Created At"
-						class="text-center"
-					/>
-					<cv-data-table-heading id="sb-assembly" heading="Assemblies" />
-					<cv-data-table-heading id="sb-result" heading="Result" />
-					<cv-data-table-heading id="sb-link" heading="Links" />
-					<cv-data-table-heading id="sb-status" heading="Status" />
-				</template>
-				<template #data>
-					<cv-data-table-row
-						:id="row.id"
-						:key="row.id"
-						:value="row.id"
-						v-for="row in data.voteEvents"
-					>
-						<cv-data-table-cell
-							><a
-								:href="`./vote-events/${row.id}`"
-								class="w-full text-inherit hover:underline"
-								>{{ row.title }}</a
-							></cv-data-table-cell
-						>
-						<cv-data-table-cell class="text-nowrap">{{
-							row.start_date
-						}}</cv-data-table-cell>
-						<cv-data-table-cell class="text-nowrap">{{
-							dayjs(row.created_at).format('YYYY-MM-DD')
-						}}</cv-data-table-cell>
-						<cv-data-table-cell>
-							<div class="flex flex-col justify-evenly">
-								<span v-for="org in row.organizations"
-									>{{ org.abbreviation }} ชุดที่ {{ org.term }}</span
+		<div v-else class="relative">
+			<div class="flex flex-col items-start gap-12 md:flex-row">
+				<VoteEventFilter
+					class="sticky top-16 w-xs"
+					v-model:filters="filters"
+					:assemblies-option="[
+						{ label: 'All assemblies', value: 'ALL' },
+						...(organizations ?? []).map((o) => ({
+							label: `${o.abbreviation} ชุดที่ ${o.term}`,
+							value: o.id,
+						})),
+					]"
+					:status-options="[
+						{ label: 'All statuses', value: 'ALL' },
+						...statusOption.map((status) => ({
+							label: status,
+							value: status,
+						})),
+					]"
+					:classification-option="[
+						...classificationOption.map((c) => ({
+							label: voteEventType[c],
+							value: c,
+						})),
+					]"
+				></VoteEventFilter>
+				<div class="w-full">
+					<cv-data-table title="Vote Events" helperText="การลงมติทั้งหมด">
+						<template #headings>
+							<cv-data-table-heading
+								id="sb-title"
+								heading="Title"
+								order="ascending"
+								class="w-2xl"
+							/>
+							<cv-data-table-heading
+								id="sb-votingdate"
+								heading="Voting Date"
+								class="text-center"
+							/>
+							<cv-data-table-heading
+								id="sb-createdat"
+								heading="Created At"
+								class="text-center"
+							/>
+							<cv-data-table-heading id="sb-assembly" heading="Assemblies" />
+							<cv-data-table-heading id="sb-result" heading="Result" />
+							<cv-data-table-heading id="sb-link" heading="Links" />
+							<cv-data-table-heading id="sb-status" heading="Status" />
+						</template>
+						<template #data>
+							<cv-data-table-row
+								:id="row.id"
+								:key="row.id"
+								:value="row.id"
+								v-for="row in data.voteEvents"
+								class="cursor-pointer"
+							>
+								<cv-data-table-cell>
+									<a
+										:href="`./vote-events/${row.id}`"
+										class="w-full text-inherit hover:underline"
+										>{{ row.title }}</a
+									>
+								</cv-data-table-cell>
+								<cv-data-table-cell class="text-nowrap">{{
+									row.start_date
+								}}</cv-data-table-cell>
+								<cv-data-table-cell class="text-nowrap">{{
+									dayjs(row.created_at).format('YYYY-MM-DD')
+								}}</cv-data-table-cell>
+								<cv-data-table-cell>
+									<div class="flex flex-col justify-evenly">
+										<span v-for="org in row.organizations"
+											>{{ org.abbreviation }} ชุดที่ {{ org.term }}</span
+										>
+									</div>
+								</cv-data-table-cell>
+								<cv-data-table-cell>{{ row.result }}</cv-data-table-cell>
+								<cv-data-table-cell>
+									<div class="flex flex-row gap-1">
+										<a
+											v-for="({ url, note }, i) in row.links"
+											:key="i"
+											:href="url"
+											target="_blank"
+											@click.stop
+										>
+											<cv-icon-button
+												kind="ghost"
+												:icon="
+													url.includes('.pdf')
+														? DocumentPdf16
+														: NotebookReference16
+												"
+												size="sm"
+												:label="note"
+											></cv-icon-button>
+										</a>
+									</div>
+								</cv-data-table-cell>
+								<cv-data-table-cell
+									:class="{
+										'text-[#DA1E28]': row.publish_status === 'ERROR',
+										'text-[#158236]': row.publish_status === 'PUBLISHED',
+									}"
+									>{{ row.publish_status }}</cv-data-table-cell
 								>
-							</div>
-						</cv-data-table-cell>
-						<cv-data-table-cell>{{ row.result }}</cv-data-table-cell>
-						<cv-data-table-cell>
-							<div class="flex flex-row gap-1">
-								<a
-									v-for="({ url, note }, i) in row.links"
-									:key="i"
-									:href="url"
-									target="_blank"
-									@click.stop
-								>
-									<cv-icon-button
-										kind="ghost"
-										:icon="
-											url.includes('.pdf') ? DocumentPdf16 : NotebookReference16
-										"
-										size="sm"
-										:label="note"
-									></cv-icon-button>
-								</a>
-							</div>
-						</cv-data-table-cell>
-						<cv-data-table-cell
-							:class="{
-								'text-[#DA1E28]': row.publish_status === 'ERROR',
-								'text-[#158236]': row.publish_status === 'PUBLISHED',
-							}"
-							>{{ row.publish_status }}</cv-data-table-cell
-						>
-					</cv-data-table-row>
-				</template>
-			</cv-data-table>
-			<ui-pagination
-				:page="paginationData.page"
-				:page-size="paginationData.pageSize"
-				:total-count="data.totalCount ?? 0"
-				:number-of-page="numberOfPage"
-				@on-page-change="handlePageChange"
-				@on-page-size-change="handlePageSizeChange"
-			/>
+							</cv-data-table-row>
+						</template>
+					</cv-data-table>
+					<ui-pagination
+						:page="paginationData.page"
+						:page-size="paginationData.pageSize"
+						:total-count="data.totalCount ?? 0"
+						:number-of-page="numberOfPage"
+						@on-page-change="handlePageChange"
+						@on-page-size-change="handlePageSizeChange"
+					/>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
