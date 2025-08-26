e146b4d316e0709d12ffcb36f23d8b04f15389b9
Th1nkK1D <witheep@gmail.com>
2025-08-26 23:40:30 +0700

    chore: remove graphql armor

--- server/routes/graphql.ts
+++ server/routes/graphql.ts
@@ -1,26 +1,12 @@
 import { ApolloServer } from '@apollo/server';
 import { startServerAndCreateH3Handler } from '@as-integrations/h3';
-import { ApolloArmor } from '@escape.tech/graphql-armor';
 import { initGraphqlNeo4j } from '~/utils/graphql/neo4j';
-
-const armor = new ApolloArmor({
-	blockFieldSuggestion: {
-		enabled: false,
-	},
-	costLimit: {
-		maxCost: 50000,
-	},
-	maxDepth: {
-		enabled: false,
-	},
-});
 
 const { neo4jgraphql } = initGraphqlNeo4j();
 
 export default startServerAndCreateH3Handler(
 	new ApolloServer({
 		schema: await neo4jgraphql.getSchema(),
-		...armor.protect(),
 		allowBatchedHttpRequests: true,
 	}),
 	{
