32d274d149d1688975ea1eea72a9816fc05515f7
Th1nkK1D <witheep@gmail.com>
2025-11-19 22:45:29 +0700

    fix: handle dev env file for auth migrate script

--- packages/auth/drizzle.config.ts
+++ packages/auth/drizzle.config.ts
@@ -4,8 +4,6 @@ if (!process.env.DATABASE_URL) {
 	throw Error('DATABASE_URL env is not provided');
 }
 
-console.log(process.env.DATABASE_URL);
-
 export default defineConfig({
 	dialect: 'postgresql',
 	schema: 'auth-schema.ts',
