adf13e2cb2bf5486d4138fa54dcb40bc03e85e10
Th1nkK1D <witheep@gmail.com>
2025-11-23 23:14:21 +0700

    feat: move tls, cors, and access control from elysia to nginx reverse proxy

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,36 +1,21 @@
-import { bearer } from '@elysiajs/bearer';
-import { cors } from '@elysiajs/cors';
 import { staticPlugin } from '@elysiajs/static';
-import { auth, trustedOrigins } from '@politigraph/auth/auth';
+import { auth } from '@politigraph/auth/auth';
 import { initNeo4jGraphql } from '@politigraph/graphql/neo4j-graphql';
 import { Elysia, type Context } from 'elysia';
 import logixlysia from 'logixlysia';
 import { apollo } from './apollo';
 
 const isProduction = process.env.NODE_ENV === 'production';
-const port = process.env.PORT ?? 3000;
 const landingSpa = Bun.file('public/index.html');
-const ca = Bun.file('tls/ca.pem');
-const cert = Bun.file('tls/cert.pem');
-const key = Bun.file('tls/key.pem');
-
-const tls =
-	(await cert.exists()) && (await key.exists())
-		? {
-				ca: (await ca.exists()) ? ca : undefined,
-				cert,
-				key,
-			}
-		: undefined;
-
-const origin = `${tls ? 'https' : 'http'}://127.0.0.1:${port}`;
+const port = process.env.PORT ?? 3000;
+const origin = `http://127.0.0.1:${port}`;
 
 const neo4jGraphql = initNeo4jGraphql(`${origin}/auth/jwks`);
 const schema = await neo4jGraphql.getSchema();
 await neo4jGraphql.checkNeo4jCompat();
 await neo4jGraphql.assertIndexesAndConstraints();
 
-const app = new Elysia({ serve: { tls } })
+const app = new Elysia()
 	.use(
 		logixlysia({
 			config: {
@@ -42,7 +27,6 @@ const app = new Elysia({ serve: { tls } })
 			},
 		}),
 	)
-	.use(cors({ origin: trustedOrigins }))
 	.use(
 		apollo({
 			schema,
@@ -69,20 +53,6 @@ const app = new Elysia({ serve: { tls } })
 		}),
 	)
 	.all('/auth/*', (ctx) => auth.handler(ctx.request));
-
-if (isProduction) {
-	app.use(bearer()).post('/auth/sign-up/email', (ctx) => {
-		if (
-			!process.env.SIGN_UP_TOKEN ||
-			ctx.bearer !== process.env.SIGN_UP_TOKEN
-		) {
-			ctx.set.status = 'Forbidden';
-			return 'Forbidden';
-		}
-
-		return auth.handler(ctx.request);
-	});
-}
 
 if (await landingSpa.exists()) {
 	app
