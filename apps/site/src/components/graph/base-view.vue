5a186d10ab9d3b06b7d29d6d30578491bc905d40
Th1nkK1D <witheep@gmail.com>
2026-01-26 18:06:23 +0700

    fix(site): reimplement graph full-screen mode

--- apps/site/src/components/graph/base-view.vue
+++ apps/site/src/components/graph/base-view.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 // @ts-ignore
-import { ChevronLeft16, ChevronRight16 } from '@carbon/icons-vue';
+import { Maximize16, Minimize16 } from '@carbon/icons-vue';
 import { type VNetworkGraphInstance } from 'v-network-graph';
-import { ref } from 'vue';
+import { nextTick, ref } from 'vue';
 
 const props = defineProps<{
 	graphElement?: VNetworkGraphInstance;
@@ -13,39 +13,40 @@ const isMaximized = ref(false);
 
 function toggleMaximize() {
 	isMaximized.value = !isMaximized.value;
-
+	nextTick();
 	props.graphElement?.fitToContents();
 	props.graphElement?.panToCenter();
 }
 </script>
 
 <template>
-	<div class="md:h-128 flex flex-col justify-end bg-gray-100 md:flex-row">
-		<div class="relative flex min-h-80 flex-col overflow-y-scroll md:flex-1">
-			<slot />
-			<div
-				class="absolute bottom-0 left-0 flex flex-row gap-2 rounded-tr bg-gray-200 p-1 text-xs text-gray-700"
-			>
-				<slot name="legend" />
-			</div>
-			<button
-				class="bottom absolute right-0 m-0 rounded-l bg-gray-800 px-0 py-1 text-white"
-				@click="toggleMaximize"
-				:aria-label="isMaximized ? 'Minimize' : 'Maximize'"
-			>
-				<ChevronLeft16 v-if="isMaximized" />
-				<ChevronRight16 v-else />
-			</button>
-		</div>
+	<Teleport to="body" :disabled="!isMaximized">
 		<div
-			class="mt-0 flex h-full flex-col gap-3 overflow-y-scroll bg-gray-800 text-white"
-			:class="
-				isMaximized
-					? 'w-0 overflow-hidden'
-					: 'max-h-80 p-3 md:max-h-none md:w-64'
-			"
+			class="flex flex-col bg-gray-100 md:flex-row"
+			:class="isMaximized ? 'fixed inset-0 z-20' : 'md:h-128'"
 		>
-			<slot name="sidebar" />
+			<div class="relative flex min-h-80 flex-col overflow-hidden md:flex-1">
+				<slot />
+				<div
+					class="absolute bottom-0 left-0 flex flex-row gap-2 rounded-tr bg-gray-200 p-1 text-xs text-gray-700"
+				>
+					<slot name="legend" />
+				</div>
+				<button
+					class="bottom absolute left-0 top-0 m-0 flex size-6 cursor-pointer items-center justify-center rounded-br bg-gray-200 text-gray-700 hover:text-black"
+					@click="toggleMaximize"
+					:aria-label="isMaximized ? 'Minimize' : 'Maximize'"
+				>
+					<Minimize16 v-if="isMaximized" />
+					<Maximize16 v-else />
+				</button>
+			</div>
+			<div
+				class="mt-0 flex h-full max-h-80 flex-col gap-3 overflow-y-scroll bg-gray-800 p-3 text-white md:max-h-none"
+				:class="isMaximized ? 'md:w-96' : 'md:w-64'"
+			>
+				<slot name="sidebar" />
+			</div>
 		</div>
-	</div>
+	</Teleport>
 </template>
